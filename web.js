361c3feebf61a8f09eb6f69bd51f598374bed99c
yaboi <tyler@tylergoelz.com>
2015-04-19 14:23:17 +0000

    Try another tut on running a node app on heroku.

---
+++ web.js
@@ -0,0 +1,10 @@
+gzippo = require 'gzippo'
+var express = require('express');
+var app = express();
+
+app.use gzippo.staticGzip "#{__dirname}/dist"
+app.set('port', (process.env.PORT || 5000));
+
+
+
+
